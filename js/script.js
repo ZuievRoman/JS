98517f3337c58b53a3352b167b45db0c6e91005a
Roman <zuevro@gmail.com>
2020-12-14 13:43:47 +0200

    Add Personal Movie Object

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 "use strict";
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count = numberOfFilms,
+    movies = new Object(),
+    actors = new Object(),
+    genres = [],
+    privat = false
+}
