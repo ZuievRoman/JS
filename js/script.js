e2c1de79f09ece7e89f7af380173a0d1dca8d241
Roman <zuevro@gmail.com>
2020-12-14 18:44:33 +0200

    Home Task

--- js/script.js
+++ js/script.js
@@ -10,18 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-/*for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних фильмов?', '');
-    const b = prompt('На сколько оцените его?', '');
-}*/
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
-const a = prompt('Один из последних фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count >= 30){
+    console.log("Вы киноман"); 
+} else{
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
