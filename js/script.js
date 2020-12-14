f3d6e6266a337b38e26539324d52f40297a54566
Roman <zuevro@gmail.com>
2020-12-14 13:57:43 +0200

    Fix error and add two variables

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-    count = numberOfFilms,
-    movies = new Object(),
-    actors = new Object(),
-    genres = [],
-    privat = false
+    count: numberOfFilms,
+    movies: new Object(),
+    actors: new Object(),
+    genres: [],
+    privat: false
 }
+
+const lastOfFilms = prompt('Один из последних фильмов?');
+const rateOfFilms = prompt('На сколько оцените его?');
